0770d1ed7d55931e913c3e01330ed931c423ef9f
igromanru <igromanru@yahoo.de>
2019-04-24 13:19:00 +0200

    added Xor class

---
+++ XorCrypt.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+
+namespace Crypt
+{
+    public class Xor
+    {
+        private static readonly Random random = new Random();
+        public static readonly int XorKey = random.Next(short.MaxValue) ^ random.Next(short.MaxValue);
+
+        public static string XorString(string text, int xorKey)
+        {
+            string result = "";
+            for (int i = 0; i < text.Length; i++)
+            {
+                var charValue = Convert.ToInt32(text[i]);
+                charValue ^= i + xorKey;
+                result += char.ConvertFromUtf32(charValue);
+            }
+            return result;
+        }
+
+        public static string XorString(string text)
+        {
+            return XorString(text, XorKey);
+        }
+    }
+}
